4f8544bf5dd2aadcb97f67d9fd96aeb5fdd30c87
DevSharma <sharma092005@gmail.com>
2025-08-06 12:56:28 +0530

    ThirdCommit

--- src/test/java/TestCases/TC006_CurrencyTest.java
+++ src/test/java/TestCases/TC006_CurrencyTest.java
@@ -1,5 +1,10 @@
 package TestCases;
 
-public class TC006_CurrencyTest {
+import org.testng.annotations.*;
 
+public class TC006_CurrencyTest {
+	@Test(groups = "tc006")
+	public void currencyTest(){
+		System.out.println("Will Do this TestLater");
+		}
 }
